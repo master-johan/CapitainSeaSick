200129df9282b116513b48e978058f065ba25f44
axlhammer <48758283+axlhammer@users.noreply.github.com>
2020-09-17 16:12:50 +0200

    hp bar implemented

---
+++ CaptainSeaSick/Assets/Healthbar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+    //REAL
+    [SerializeField] private Image foreGroundImage;
+    [SerializeField] private float updateSpeedSeconds = 0.5f;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        //GetComponentInParent<ShipHealth>().healthPctChanged += HealthChange;
+        GameObject.FindGameObjectWithTag("Ship").GetComponent<ShipHealth>().healthPctChanged += HealthChange;
+    }
+    private void HealthChange(float percent)
+    {
+        //foreGroundImage.fillAmount = percent;
+        StartCoroutine(ChangeToPercent(percent));
+    }
+    private IEnumerator ChangeToPercent(float percent)
+    {
+        float tempChangePercent = foreGroundImage.fillAmount;
+        float tempTime = 0f;
+
+        while (tempTime < updateSpeedSeconds)
+        {
+            tempTime += Time.deltaTime;
+            foreGroundImage.fillAmount = Mathf.Lerp(tempChangePercent, percent, tempTime / updateSpeedSeconds);
+            yield return null;
+        }
+        foreGroundImage.fillAmount = percent;
+    }
+}
---
+++ CaptainSeaSick/Assets/ShipHealth.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipHealth : MonoBehaviour
+{
+    [SerializeField]
+    private float maxHealth = 100;
+    public float currenthealth;
+
+    public event Action <float> healthPctChanged = delegate { };
+    private void OnEnable()
+    {
+        currenthealth = maxHealth;
+    }
+
+    public void ModifyHealth(float amount)
+    {
+        currenthealth += amount;
+        float currentHeathPct = currenthealth / maxHealth;
+        healthPctChanged(currentHeathPct);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        ModifyHealth(-10);
+    }
+   
+       
+   
+}

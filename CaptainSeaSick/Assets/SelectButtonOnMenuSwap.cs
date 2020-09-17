987c8e1dda0306a18362a5b93c2048508eedb2c6
kallesongotto <47325654+kallesongotto@users.noreply.github.com>
2020-09-17 15:26:12 +0200

    Main Menu
    Fixed mainmenu controlled with Xbox controller. Can change between two menus

---
+++ CaptainSeaSick/Assets/SelectButtonOnMenuSwap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.UI;
+using UnityEngine.UI;
+
+public class SelectButtonOnMenuSwap : MonoBehaviour
+{
+    public EventSystem eventSystem;
+
+    public GameObject selectedButton;
+    bool once;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SelectButton()
+    {
+        eventSystem.SetSelectedGameObject(selectedButton);
+    }
+}

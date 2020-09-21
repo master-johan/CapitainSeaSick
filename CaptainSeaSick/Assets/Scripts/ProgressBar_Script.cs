56c01f589372ff1cd93d0eaa1e47fc9435e6d360
axlhammer <48758283+axlhammer@users.noreply.github.com>
2020-09-21 15:23:00 +0200

    TimelineWIP
    -Timeline + script
    //axel

--- CaptainSeaSick/Assets/Scripts/ProgressBar_Script.cs
+++ CaptainSeaSick/Assets/Scripts/ProgressBar_Script.cs
@@ -9,8 +9,11 @@ using UnityEngine.UI;
 public class ProgressBar_Script : MonoBehaviour
 {
     private Slider slider;
-    public float levelTime = 120f; //2 MIN
-    public float timeLeft; //How long left
+    private float levelTime = 120f; //2 MIN
+    private float timeLeft; //How long left
+
+    public float TimeLeft { get => timeLeft; set => timeLeft = value; }
+    public float LevelTime { get => levelTime; set => levelTime = value; }
 
     private void Awake()
     {
@@ -19,28 +22,21 @@ public class ProgressBar_Script : MonoBehaviour
     void Start()
     {
         
-        timeLeft = levelTime;
+        timeLeft = LevelTime;
         slider.value = timeLeft;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-
         if (timeLeft > 0)
         {
             timeLeft -= Time.deltaTime;
-            slider.value = timeLeft / levelTime;
+            slider.value = timeLeft / LevelTime;
         }
         else
         {
             Time.timeScale = 0;
         }      
-    }
-
-    public void IncrementProgress(float newProgress)
-    {
-        slider.value += newProgress;
     }
 }

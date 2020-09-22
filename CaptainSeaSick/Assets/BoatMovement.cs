bf59bd5b1216fbeb1925ab30b8abd79451c29125
kallesongotto <47325654+kallesongotto@users.noreply.github.com>
2020-09-22 17:02:50 +0200

    Progress on "dodge" mechanic

---
+++ CaptainSeaSick/Assets/BoatMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BoatMovement : MonoBehaviour
+{
+    public SteeringScript steeringScript;
+    public InputActionMap shipActionInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shipActionInput.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(steeringScript.GetPlayer() != null)
+        //{
+           // Debug.Log(steeringScript.GetPlayer());
+
+            if (shipActionInput.FindAction("StickToSteeringPos").triggered && steeringScript.GetSteeringBool())
+            {
+                //steeringScript.GetPlayer().transform.SetParent(transform);
+
+                if (shipActionInput.FindAction("MoveUp").triggered && steeringScript.GetSteeringBool())
+                {
+                    gameObject.GetComponent<Rigidbody>().MovePosition(transform.position + new Vector3(0, 0, 1));
+
+                }
+                if (shipActionInput.FindAction("MoveDown").triggered && steeringScript.GetSteeringBool())
+                {
+                    gameObject.GetComponent<Rigidbody>().MovePosition(transform.position + new Vector3(0, 0, -1));
+
+                }
+            }
+        //}
+         
+    }
+
+}
---
+++ CaptainSeaSick/Assets/SteeringScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SteeringScript : MonoBehaviour
+{
+
+    bool inSteeringPosition;
+    GameObject player;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Boolen = " + inSteeringPosition);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            player = other.gameObject;
+            inSteeringPosition = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inSteeringPosition = false;
+        }
+    }
+
+    public bool GetSteeringBool()
+    {
+        return inSteeringPosition;
+    }
+    public GameObject GetPlayer()
+    {
+        return player;
+    }
+}

e206933b0e4a551e748c397879f764d11dd964a9
axlhammer <48758283+axlhammer@users.noreply.github.com>
2020-10-19 12:24:55 +0200

    ny båt

---
+++ CaptainSeaSick/Assets/Scripts/Steering_Trigger_Script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steering_Trigger_Script : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player Enter Steering Zone");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player Exit Steering Zone");
+        }
+    }
+}
---
+++ CaptainSeaSick/Assets/TriggerDoor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDoor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Player Enter");
+            GameObject.Find("DoorFront").GetComponent<Door_Script>().OpenDoor();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player Exit");
+            GameObject.Find("DoorFront").GetComponent<Door_Script>().CloseDoor();
+        }
+    }
+}

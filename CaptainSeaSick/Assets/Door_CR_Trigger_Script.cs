4cdec41510918786676084a51ebd4b2ba46a0d4e
axlhammer <48758283+axlhammer@users.noreply.github.com>
2020-11-09 12:58:48 +0100

    Fucking dörrjävlar
    Nya skript och funktiuonalliteter
    av kevva och axel

---
+++ CaptainSeaSick/Assets/Door_CR_Trigger_Script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_CR_Trigger_Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "CaptainsKey")
+        {
+            Debug.Log("KEY ENTER");
+            GameObject.Find("DoorPivot_CR").GetComponent<Door_CR_Script>().OpenDoor();
+        }
+       
+    }
+}
--- CaptainSeaSick/Assets/TriggerDoor.cs
+++ CaptainSeaSick/Assets/TriggerDoor.cs
@@ -7,29 +7,44 @@ public class TriggerDoor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player")
+        if (other.tag == "Player")
         {
-            Debug.Log("Player Enter");
-            GameObject.Find("DoorPivot").GetComponent<Door_Script>().OpenDoor();
+            if (!other.isTrigger)
+            {
+                Debug.Log("Player Enter");
+                GameObject.Find("DoorPivot").GetComponent<Door_Script>().OpenDoor();
+            }
+
         }
     }
+
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    if (other.tag == "Player")
+    //    {
+    //        GameObject.Find("DoorPivot").GetComponent<Door_Script>().StayOpen();
+    //    }
+    //}
 
     private void OnTriggerExit(Collider other)
     {
         if (other.tag == "Player")
         {
-            Debug.Log("Player Exit");
-            GameObject.Find("DoorPivot").GetComponent<Door_Script>().CloseDoor();
+            if(!other.isTrigger)
+            {
+                Debug.Log("Player Exit");
+                GameObject.Find("DoorPivot").GetComponent<Door_Script>().CloseDoor();
+            }
         }
     }
 }

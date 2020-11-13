8f72ed8efdc65c918de694fc5d2dd04f6911263d
JeppaVansinnig <Jesper.larsson1999@gmail.com>
2020-11-13 14:21:03 +0100

    HeatMap

--- CaptainSeaSick/Assets/Scripts/HeatMap/Grid.cs
+++ CaptainSeaSick/Assets/Scripts/HeatMap/Grid.cs
@@ -1,18 +1,68 @@
-﻿using System.Collections;
+﻿using CodeMonkey.Utils;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float cellSize;
+
+    private int width;
+    private int height;
+    private int[,] gridArray;
+    private TextMesh[,] debugTextArray;
+    public Grid(int width, int height, float cellSize)
     {
-        
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+
+        gridArray = new int[width, height];
+        debugTextArray = new TextMesh[width, height];
+
+        Debug.Log(width + "  " + height);
+
+        for (int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                debugTextArray[x,y] = UtilsClass.CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPosition(Mathf.FloorToInt(transform.position.x) + x, Mathf.FloorToInt(transform.position.y) + y) + new Vector3(cellSize,cellSize) * 0.5f, 20, Color.white, TextAnchor.MiddleCenter);
+                Debug.DrawLine(GetWorldPosition(Mathf.FloorToInt(transform.position.x) + x, Mathf.FloorToInt(transform.position.y) + y), GetWorldPosition(Mathf.FloorToInt(transform.position.x) + x, Mathf.FloorToInt(transform.position.y) + y + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(Mathf.FloorToInt(transform.position.x) + x, Mathf.FloorToInt(transform.position.y) + y), GetWorldPosition(Mathf.FloorToInt(transform.position.x) + x + 1, Mathf.FloorToInt(transform.position.y) + y), Color.white, 100f);
+            }
+        }
+
+        Debug.DrawLine(GetWorldPosition(width, (Mathf.FloorToInt(transform.position.y))), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition((Mathf.FloorToInt(transform.position.x)), height), GetWorldPosition(width, height), Color.white, 100f);
+
+        SetValue(2, 1, 56);
     }
 
-    // Update is called once per frame
-    void Update()
+    private Vector3 GetWorldPosition(int x, int y)
     {
-        
+        return new Vector3(x, y) * cellSize;
+    }
+    
+    private void GetXY(Vector3 worldPosition, out int x, out int y)
+    {
+        x = Mathf.FloorToInt(worldPosition.x / cellSize);
+        y = Mathf.FloorToInt(worldPosition.y / cellSize);
+    }
+
+    public void SetValue(int x, int y, int value)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            gridArray[x, y] = value;
+            debugTextArray[x, y].text = gridArray[x, y].ToString();
+        }
+    }
+
+    public void SetValue(Vector3 worldPosition, int value)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetValue(x, y, value);
     }
 }
---
+++ CaptainSeaSick/Assets/Scripts/HeatMap/GridTest.cs
@@ -0,0 +1,29 @@
+﻿using CodeMonkey.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTest : MonoBehaviour
+{
+    GameObject[] players;
+    int a;
+    private Grid grid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = new Grid(10, 2, 10f);
+        a = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        a += 1;
+        players = GameObject.FindGameObjectsWithTag("Player");
+        if (players != null)
+        {
+            grid.SetValue(new Vector3(players[0].transform.position.x, players[0].transform.position.z, 0), a);
+            Debug.Log(players[0].transform.position);
+        }
+    }
+}

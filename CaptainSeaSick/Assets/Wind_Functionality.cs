a1217d8b224affb2da24470ebae025912942c364
kallesongotto <47325654+kallesongotto@users.noreply.github.com>
2020-11-13 13:10:21 +0100

    Testing out windy bois

---
+++ CaptainSeaSick/Assets/Wind_Functionality.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind_Functionality : MonoBehaviour
+{
+
+    GameObject[] players;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+
+        players = GameObject.FindGameObjectsWithTag("Player");
+
+        if (GameAssets.instance.windActivated)
+        {
+
+            foreach (var item in players)
+            {
+
+                if (item.GetComponent<PlayerActions>().isBoosting || item.GetComponent<PlayerActions>().isStunned)
+                {
+                }
+                else
+                {
+                    item.GetComponent<Rigidbody>().AddForce(transform.forward, ForceMode.VelocityChange);
+                }
+
+
+
+                //item.GetComponent<Rigidbody>().velocity += transform.forward;
+            }
+
+        }
+    }
+}

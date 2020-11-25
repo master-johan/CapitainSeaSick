6dcc48f0febcbe2f0dd9733bf08cfb77f3c795f2
Keffat <Kevin21magnusson@hotmail.com>
2020-11-25 13:06:18 +0100

    WeatherManager without the integration to the levelmanager.

---
+++ CaptainSeaSick/Assets/Scripts/Weather/CloudSwitch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSwitch : MonoBehaviour
+{
+    public ParticleSystem heavyClouds, lightClouds;
+    public bool startHeavyClouds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (startHeavyClouds)
+        {
+            if (!heavyClouds.isPlaying)
+            {
+               heavyClouds.Play();
+            }
+        }
+        else
+        {
+            heavyClouds.Stop();
+        }
+    }
+}
---
+++ CaptainSeaSick/Assets/Scripts/Weather/RainSwitch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainSwitch : MonoBehaviour
+{
+    public bool rainOn;
+    ParticleSystem rain;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rain = transform.GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rainOn)
+        {
+            rain.Play();
+        }
+        else
+        {
+            rain.Stop();
+        }
+    }
+}

09e8da863b52afa5c4e7a12f1492c80e98a60df9
Keffat <Kevin21magnusson@hotmail.com>
2020-11-26 12:17:45 +0100

    A particle trail for boosting, a patricle effect for when getting hit by enemy cannonballs!

--- CaptainSeaSick/Assets/Scripts/Player & Controller/PlayerTrail_Script.cs
+++ CaptainSeaSick/Assets/Scripts/Player & Controller/PlayerTrail_Script.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeatherManager_Script : MonoBehaviour
+public class PlayerTrail_Script : MonoBehaviour
 {
-    public ParticleSystem rain, cloud;
-    public GameObject light, wind;
+
+    ParticleSystem ps;
     // Start is called before the first frame update
     void Start()
     {
+        ps.GetComponent<ParticleSystem>();
+        ParticleSystem.MainModule ma = ps.main;
 
+       // ma.startColor = new Color...
     }
 
     // Update is called once per frame

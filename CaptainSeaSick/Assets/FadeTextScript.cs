91c5d22a9e242aa911d486c0edbd77d6c7f8ea72
Otto <kallesongotto@gmail.com>
2020-12-16 15:42:39 +0100

    WinningScreen

---
+++ CaptainSeaSick/Assets/FadeTextScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FadeTextScript : MonoBehaviour
+{
+    public TextMeshProUGUI Text;
+    public Color myColor;
+
+    bool tint;
+    float blinkSpeed;
+    void Start()
+    {
+        blinkSpeed = 0.007f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (myColor.a <= 0f)
+        {
+            tint = false;
+        }
+        else if (myColor.a >= 1f)
+        {
+            tint = true;
+        }
+
+
+        if (tint)
+        {
+            myColor.a -= blinkSpeed;
+        }
+        else
+        {
+            myColor.a += blinkSpeed;
+        }
+        //Text.GetComponent<Renderer>().material.SetColor("_Color", myColor);
+
+        Text.color = myColor;
+    }
+}
---
+++ CaptainSeaSick/Assets/FireWorkMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireWorkMovement : MonoBehaviour
+{
+    float timer, lifeTimer;
+    public GameObject boom;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ParticleSystem.MainModule settings = boom.GetComponent<ParticleSystem>().main;
+        settings.startColor = new ParticleSystem.MinMaxGradient(new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)));
+
+
+
+        timer = Random.Range(1f, 3f);
+
+        lifeTimer = timer + 4;
+        for (int i = 0; i < boom.GetComponent<ParticleSystem>().emission.burstCount; i++)
+        {
+            boom.GetComponent<ParticleSystem>().emission.SetBurst(i, new ParticleSystem.Burst(timer, 500));
+         
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timer -= Time.deltaTime;
+        lifeTimer -= Time.deltaTime;
+        if (timer > 0)
+        {
+            transform.position += new Vector3(0, 50, 0) * Time.deltaTime;
+        }
+
+        if(lifeTimer <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+
+    }
+}
